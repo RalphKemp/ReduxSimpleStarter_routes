3cb007474627a2bc766e9024c4b7340313da31de
ralph kemp <ralphk3mp@gmail.com>
2018-02-10 23:32:42 +0000

    added a submit button and the data seems to be working in the console log, will continue to improve

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,14 +12,26 @@ class PostsNew extends Component {
         type="text"
           {...field.input}
          />
+      {field.meta.error}
       </div>
     );
   }
 
+  onSubmit(values) {
+    // this === component
+    console.log(values);
+  }
+
 
   render() {
+    const { handleSubmit } = this.props;
+    // this is a property that is being passed to the component on behalf of redux form
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+      {/*redux from is just responsible for handling the state and validation of our form*/}
+      {/*so handle submit is from redux form and it says if everything with the form side like validation and state are ok, we */}
+      {/*then go ahead and run the call back this.onsubmit*/}
         <Field
           label="Title"
           name="title"
@@ -35,6 +47,7 @@ class PostsNew extends Component {
           name="content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">submit</button>
       </form>
     );
   }
@@ -53,7 +66,6 @@ function validate(values) {
   if (!values.content) {
     errors.content = "enter some content!";
   }
-
 
   // if errors is empty, the form is fine to submit
   // if errors has any properties redux form assumes form is invalid
