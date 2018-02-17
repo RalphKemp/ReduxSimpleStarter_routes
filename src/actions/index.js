12b1468dbb9de443319b379687f2fa37c9ff5607
ralph kemp <ralphk3mp@gmail.com>
2018-02-17 23:26:00 +0000

    create new post working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
-
+export const CREAT_POST = 'create_post';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
-const API_KEY = '?key=123';
+const API_KEY = '?key=fweg';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values) {
+   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 
@@ -26,7 +29,7 @@ class PostsNew extends Component {
 
   onSubmit(values) {
     // this === component
-    console.log(values);
+    this.props.createPost(values);
   }
 
 
@@ -55,6 +58,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -83,4 +87,13 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{ createPost })(PostsNew)
+);
+
+
+
+
+
+
+
